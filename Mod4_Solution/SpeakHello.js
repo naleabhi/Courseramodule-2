2be8d7659346078e8eb58ff8791e8d8e2b97f34b
naleabhi <abhijeetnale82@gmail.com>
2020-09-17 11:11:33 +0530

    my fourth Assignment.

--- Mod4_Solution/SpeakHello.js
+++ Mod4_Solution/SpeakHello.js
@@ -7,13 +7,17 @@
 // See Lecture 52, part 1
 // var helloSpeaker =
 
+( function(window)
+{
+var helloSpeaker={};
+
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
 var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+ helloSpeaker.speak=function(name) {
   console.log(speakWord + " " + name);
 }
 
@@ -22,3 +26,6 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+window.helloSpeaker=helloSpeaker;
+
+})(window);
